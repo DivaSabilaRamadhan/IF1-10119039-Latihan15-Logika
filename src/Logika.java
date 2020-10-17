d8286eb84f071314792cf6a8805da8441bc419d8
diky <diky@diky-PC>
2020-10-17 14:18:00 +0700

    Upload Project

---
+++ src/Logika.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * NAMA  : Diva Sabila Ramadhan 
+ * KELAS : IF1
+ * NIM   : 10119039
+ * Deskripsi Program : Program Logika 
+ */
+public class Logika {
+
+   
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("1a = " + !a);
+        System.out.println("!(a && b) = " + ! (a && b));
+    }
+    
+}
